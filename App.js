d4b2bc073f26d6181b1d90c6760a9aaeb57b14ff
Darshan Revankar <darshanjbdvt24@gmail.com>
2025-04-21 20:52:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import TodoInput from './components/TodoInput'
+const App = () => {
+  return (
+    <div className='MainContainer'>
+      <div className='CenterContainer'>
+          <TodoInput/>
+      </div>
+    </div>
+
+     
+  )
+}
+
+export default App
